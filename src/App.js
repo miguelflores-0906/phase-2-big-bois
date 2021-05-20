475ed8e92129410c1375869c32f28dfef82b2838
miguelflores-0906 <miggy_4th@yahoo.com>
2021-05-21 03:28:09 +0800

    stable so far need eugene login and navbar

--- src/App.js
+++ src/App.js
@@ -8,43 +8,20 @@ import Dashboard from './components/Dashboard'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Boardpost from './components/Boardpost'
 import Postpage from './components/Postpage'
-import Newpost from './components/Newpost'
 import Login from './components/Login'
+import Newpost from './components/Newpost'
 
 function App() {
   return (
     <Router>
       <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        
         {/* <Postpage /> */}
         <Switch>
+          <Route path="/" exact component={Login}/>
           <Route path="/newpost" exact component ={Newpost}/>
-=======
-        <Navbar />
-        {/* <Postpage /> */}
-        <Switch>
-          <Route path="/" exact component ={Newpost}/>
->>>>>>> parent of 822a359 (Merge branch 'master' into DyDB)
-=======
-        <Navbar />
-        {/* <Postpage /> */}
-        <Switch>
-          <Route path="/" exact component ={Newpost}/>
->>>>>>> parent of 822a359 (Merge branch 'master' into DyDB)
-=======
-        <Navbar />
-        {/* <Postpage /> */}
-        <Switch>
-          <Route path="/" exact component ={Newpost}/>
->>>>>>> parent of 822a359 (Merge branch 'master' into DyDB)
           <Route path="/dashboard" exact component={Dashboard} />
           <Route path="/board/" exact component={Board} />
           <Route path="/profile" exact component={Profile} />
-          <Route path="/navbar" exact component = {Navbar} />
-          <Route path="/login" exact component = {Login} />
         </Switch>
       </div>
     </Router>
