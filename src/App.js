ea87032b4ac78c64479722f03c4de68118daadef
miguelflores-0906 <miggy_4th@yahoo.com>
2021-05-21 03:12:55 +0800

    ...

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ import Dashboard from './components/Dashboard'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Boardpost from './components/Boardpost'
 import Postpage from './components/Postpage'
-import Login from './components/Login'
 import Newpost from './components/Newpost'
+import Login from './components/Login'
 
 function App() {
   return (
     <Router>
       <div>
+        
         {/* <Postpage /> */}
         <Switch>
-          <Route path="/" exact component={Login}/>
           <Route path="/newpost" exact component ={Newpost}/>
           <Route path="/dashboard" exact component={Dashboard} />
           <Route path="/board/" exact component={Board} />
           <Route path="/profile" exact component={Profile} />
+          <Route path="/navbar" exact component = {Navbar} />
+          <Route path="/login" exact component = {Login} />
         </Switch>
       </div>
     </Router>
